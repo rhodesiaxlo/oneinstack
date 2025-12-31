fcbcfb5ae6fe19f3c0fdff2e0776c3959f51c997
sennalo <lsx_tedybear@126.com>
2025-12-31 15:56:10 +0800

    123

--- include/check_download.sh
+++ include/check_download.sh
@@ -110,7 +110,7 @@ checkDownload() {
     src_url=http://archive.apache.org/dist/apr/apr-${apr_ver}.tar.gz && Download_src
   fi
 
-  if [[ "${db_option}" =~ ^[1-9]$|^1[0-4]$ ]]; then
+  if [[ "${db_option}" =~ ^[0-9]$|^1[0-4]$ ]]; then
     if [[ "${db_option}" =~ ^[1,2,5,6,7,9]$|^10$ ]] && [ "${dbinstallmethod}" == "2" ]; then
       [[ "${db_option}" =~ ^[2,5,6,7]$|^10$ ]] && boost_ver=${boost_oldver}
       [[ "${db_option}" =~ ^9$ ]] && boost_ver=${boost_percona_ver}
@@ -121,6 +121,38 @@ checkDownload() {
     fi
 
     case "${db_option}" in
+      0)
+        if [ "${OUTIP_STATE}"x == "China"x ]; then
+          DOWN_ADDR_MYSQL=https://cdn.mysql.com/Downloads/MySQL-8.2
+          DOWN_ADDR_MYSQL_BK=https://mirrors.aliyun.com/mysql/MySQL-8.2
+          DOWN_ADDR_MYSQL_BK2=http://mirrors.tuna.tsinghua.edu.cn/mysql/downloads/MySQL-8.2
+        else
+          DOWN_ADDR_MYSQL=https://cdn.mysql.com/Downloads/MySQL-8.2
+          DOWN_ADDR_MYSQL_BK=https://mirrors.dotsrc.org/mysql/Downloads/MySQL-8.2
+        fi
+
+        if [ "${dbinstallmethod}" == '1' ]; then
+          echo "Download MySQL 8.2 binary package..."
+          FILE_NAME=mysql-${mysql82_ver}-linux-glibc2.17-x86_64.tar.xz
+        elif [ "${dbinstallmethod}" == '2' ]; then
+          echo "Download MySQL 8.2 source package..."
+          FILE_NAME=mysql-${mysql82_ver}.tar.gz
+        fi
+        src_url=${DOWN_ADDR_MYSQL}/${FILE_NAME} && Download_src
+        src_url=${DOWN_ADDR_MYSQL}/${FILE_NAME}.md5 && Download_src
+        MYSQL_TAR_MD5=$(awk '{print $1}' ${FILE_NAME}.md5)
+        [ -z "${MYSQL_TAR_MD5}" ] && MYSQL_TAR_MD5=$(curl -s ${DOWN_ADDR_MYSQL_BK}/${FILE_NAME}.md5 | grep ${FILE_NAME} | awk '{print $1}')
+        tryDlCount=0
+        while [ "$(md5sum ${FILE_NAME} | awk '{print $1}')" != "${MYSQL_TAR_MD5}" ]; do
+          wget -c --no-check-certificate ${DOWN_ADDR_MYSQL_BK}/${FILE_NAME};sleep 1
+          let "tryDlCount++"
+          [ "$(md5sum ${FILE_NAME} | awk '{print $1}')" == "${MYSQL_TAR_MD5}" -o "${tryDlCount}" == '6' ] && break || continue
+        done
+        if [ "${tryDlCount}" == '6' ]; then
+          echo "${CFAILURE}${FILE_NAME} download failed, Please contact the author! ${CEND}"
+          kill -9 $$; exit 1;
+        fi
+        ;;
       1)
         # MySQL 8.0
         if [ "${OUTIP_STATE}"x == "China"x ]; then
--- include/jemalloc.sh
+++ include/jemalloc.sh
@@ -22,7 +22,8 @@ Install_Jemalloc() {
       else
         ln -s /usr/local/lib/libjemalloc.so.2 /usr/lib/libjemalloc.so.1
       fi
-      [ -z "`grep /usr/local/lib /etc/ld.so.conf.d/*.conf`" ] && echo '/usr/local/lib' > /etc/ld.so.conf.d/local.conf
+      [ ! -d /etc/ld.so.conf.d ] && mkdir -p /etc/ld.so.conf.d
+      grep -qs "/usr/local/lib" /etc/ld.so.conf.d/*.conf 2>/dev/null || echo '/usr/local/lib' > /etc/ld.so.conf.d/local.conf
       ldconfig
       echo "${CSUCCESS}jemalloc module installed successfully! ${CEND}"
       rm -rf jemalloc-${jemalloc_ver}
